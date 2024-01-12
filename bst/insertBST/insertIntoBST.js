57fbc047fa8205ebc2ea7d3e5e77384081fe678b
Quentin Gibson <quentingibson94@gmail.com>
2024-01-11 21:46:09 -0500

    Add insertIntoBST function to insert a node into a binary search tree

--- bst/insertBST/insertIntoBST.js
+++ bst/insertBST/insertIntoBST.js
@@ -1,3 +1,16 @@
-export function insertIntoBST() {
-  return 'Not yet implemented'
+class Node {
+  constructor(val, left, right) {
+    this.val = val
+    this.left = left
+    this.right = right
+  }
+}
+export function insertIntoBST(root, key) {
+  if (!root) return { val: key, left: null, right: null }
+  if (key > root.val) {
+    root.right = insertIntoBST(root.right, key)
+  } else if (key < root.val) {
+    root.left = insertIntoBST(root.left, key)
+  }
+  return root
 }
