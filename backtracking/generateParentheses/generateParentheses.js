4aba209dacc2f7757967c2a92df4261ecf80f598
Quentin Gibson <quentingibson94@gmail.com>
2024-01-15 15:34:15 -0500

    Add generateParentheses and

--- backtracking/generateParentheses/generateParentheses.js
+++ backtracking/generateParentheses/generateParentheses.js
@@ -1,8 +1,24 @@
+
 export function generateParentheses(n) {
   let ans = []
-  function dfs(start, path) {
+  function dfs(start, path, start_count, end_count) {
+    if (start === n * 2) {
+      ans.push(path.join(""))
+      return
+    }
 
+    if (start_count < n) {
+      path.push("(")
+      dfs(start + 1, path, start_count + 1, end_count)
+      path.pop()
+    }
+
+    if (end_count < start_count) {
+      path.push(")")
+      dfs(start + 1, path, start_count, end_count + 1)
+      path.pop()
+    }
   }
-  dfs(0, [])
+  dfs(0, [], 0, 0)
   return ans
 }
